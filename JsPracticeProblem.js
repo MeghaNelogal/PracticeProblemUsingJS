7c7c10092d62387fd97b0a6b5a308bdb0ddb8a79
Your Name <you@example.com>
2022-09-14 13:12:14 +0530

    [Megha] Add: Read Number and Display Week

--- JsPracticeProblem.js
+++ JsPracticeProblem.js
@@ -1,23 +1,36 @@
 
-//UC7
+//UC8
 
 var Val1=1;
 var Val2=2;
 var Val3=3;
 var Val4=4;
-let x=1;
+var Val5=5;
+var Val6=6;
+var Val7=7;
+let x=5;
+
 switch(x)
-{
+{   
     case Val1:
-        console.log("One");
+        console.log("Sunday");
         break;
         case Val2:
-            console.log("Two");
+            console.log("Monday");
         break;
     case Val3:
-            console.log("Three");
+            console.log("Tuesday");
         break;
     case Val4:
-            console.log("Four");
+            console.log("Wednesday");
+         break;
+         case Val5:
+            console.log("thrusday");
+         break;
+         case Val6:
+            console.log("Friday");
+         break;
+         case Val7:
+            console.log("Saturday");
          break;
 }
