8cf4c640ad0e66aab474cd08092037f0510a0157
Your Name <you@example.com>
2022-09-14 12:52:14 +0530

    [Megha] Add: Check Head or Tail

--- JsPracticeProblem.js
+++ JsPracticeProblem.js
@@ -1,10 +1,11 @@
 
-//UC3
+//UC4
 
-var year=2022;
-if(((year%4==0)&&(year%100!=0))||(year%400==0))
+var randVal=Math.floor(Math.random()*10)%2;
+
+if(randVal==0)
 {
-  console.log(year+" is leap year");
+  console.log("Head");
 }else{
-  console.log(year+" is not leap year");
+  console.log("tail");
 }
